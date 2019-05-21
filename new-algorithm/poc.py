12fe2d06f577b9acbe7dd029a2ed4691ca2f9b08
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2019-05-21 08:15:41 -0300

    Add networkx to ease the PoC

--- new-algorithm/poc.py
+++ new-algorithm/poc.py
@@ -1,23 +1,33 @@
-def get_triangles():
+import networkx as nx
+
+def compute_triangles(graph):
+    return nx.triangles(graph) # TODO divide by 3
+
+def get_t_n():
+    pass
     # TODO
+
+def verify_clique(graph):
+    n = graph.number_of_nodes()
+    return graph.number_of_edges() == n * (n - 1) / 2  
 
 
 def explore (node, father, graph):
-    graph := node.neighbors \intersection graph 
+    graph = graph.keep_only_neighbors(node) 
     triangles = compute_triangles (graph)
     clique = verify_clique (graph)
-    (k_triangles,next_neighbor) = get_t_n (triangles)
-    while ( k_triangles > |clique|  ||  next_neighbor != NUL ) do
+    k_triangles, next_neighbor = get_t_n (triangles)
+    while k_triangles > len(clique) or next_neighbor != None:
         clique = explore (next_neighbor, node, graph)
         (k_triangles,next_neighbor) = get_t_n (triangles) 
-        return (clique)
+        return clique
 
 def main (graph):
-    # graph := compute_2-core (graph)   # esto es una optimización 
-    #     mclique := {}
+    # graph := compute_2-core (graph)   # esto es una optimizacion 
+    max_clique = set()
     for node in graph:
         (k_triangles,next_neighbor) = get_t_n (triangles)
         clique = explore (node, node, graph)
-        if (|clique| > |mclique|):
-            mclique := clique
-    return (mclique)
+        if (len(clique) > len(max_clique)):
+            max_clique = clique
+    return max_clique
