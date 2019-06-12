f3b4e91165831c5065c0a244760e798ed87b3320
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-06-11 23:51:08 -0300

    some cleaning and more prints

--- pattern-algorithm/pattern.py
+++ pattern-algorithm/pattern.py
@@ -1,30 +1,29 @@
+import time
 import networkx as NX
 
-def explore(G, U, C, low, max_clique, prev):
+def explore(G, U, C, low, max_clique, level):
 
     if len(U) == 0 and len(C) > max_clique:
         return
 
     while len(U) > 0:
 
-        if prev + len(U) <= len(C):
+        if level + len(U) <= len(C):
             return
 
-        i = min(U)
-        
         Uc = U.copy()
         
-        Uc.remove(i)
-        C.add(i)
+        v = Uc.pop()
+        C.add(v)
 
         Np = set()
-        vngbs = G.neighbors(i)
+        vngbs = G.neighbors(v)
 
         for wj in vngbs:
             if G.degree(wj) >= max_clique:
                 Np.add(wj)
 
-        explore(G, Uc.intersection(Np), C, low, max_clique, prev + 1)
+        explore(G, Uc.intersection(Np), C, low, max_clique, level + 1)
 
 def max_clique(G):
 
@@ -61,11 +60,23 @@ def max_clique(G):
 
 def main():
 
-    G = NX.complete_graph(4)
-
+    G = NX.complete_graph(6)
+    
+    start = time.time()
     clique = max_clique(G)
+    end = time.time()
 
-    print("final result: {}".format(clique))
+    print("Result: {}".format(clique))
+    
+    delta_own = end - start
+
+    start = time.time()
+    NX.graph_clique_number(G)
+    end = time.time()
+
+    delta_nx = end - start
+
+    print("Time - Own: {}, NX: {}".format(delta_own, delta_nx))
 
 if __name__ == "__main__":
     main()
