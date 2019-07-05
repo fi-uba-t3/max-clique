623a2b35280e53e98fc3dc235edc02e53c7db024
Patricio Iribarne Catella <patricio_14_94@hotmail.com>
2019-07-05 20:11:33 -0300

    Fixes in the parallel version

--- pattern-algorithm/pattern-distr.py
+++ pattern-algorithm/pattern-distr.py
@@ -4,14 +4,20 @@ import networkx as NX
 
 from multiprocessing import Process, Queue, Value
 
-def explore(G, U, C, max_clique, level):
+def explore(wid, G, U, C, max_clique, level):
 
-    if len(U) == 0 and len(C) > max_clique.value:
+    print("wid: {}, U: {}, C: {}, max_clique: {}, level: {}".format(
+                wid, U, C, max_clique.value, level))
+
+    if len(U) == 0:
+        with max_clique.get_lock():
+            if level > max_clique.value:
+                max_clique.value = level
         return
 
     while len(U) > 0:
 
-        if level + len(U) <= len(C):
+        if level + len(U) <= max_clique.value:
             return
 
         Uc = U.copy()
@@ -26,7 +32,7 @@ def explore(G, U, C, max_clique, level):
             if G.degree(wj) >= max_clique.value:
                 Np.add(wj)
 
-        explore(G, Uc.intersection(Np), C, max_clique, level + 1)
+        explore(wid, G, Uc.intersection(Np), C, max_clique, level + 1)
 
 def calc_max_clique(wid, q_in, q_out, val, G):
 
@@ -43,29 +49,29 @@ def calc_max_clique(wid, q_in, q_out, val, G):
 
         U, C = item
 
-        print("U: {}, C: {}".format(U, C))
-
         # check if the vertexes
         # verify the clique condition
 
-        for v in U:
-            if G.degree(v) < val.value:
-                U.remove(v)
+        with val.get_lock():
+            U = set(filter(lambda x: G.degree(x) >= val.value, U))
 
-        explore(G, U, C, val, 1)
+        print("wid: {}, U: {}, C: {}".format(wid, U, C))
+        
+        explore(wid, G, U, C, val, 1)
 
-        if len(C) > val.value:
-            with val.get_lock():
+        print("wid: {}, C: {}".format(wid, C))
+
+        with val.get_lock():
+            if len(C) >= val.value:
                 val.value = len(C)
                 res = C.copy()
 
-        print("max_clique: {}".format(val.value))
+        print("wid: {}, res: {}".format(wid, res))
+        print("wid: {}, max_clique: {}".format(wid, val.value))
     
     q_out.put(res)
 
-def test_graph(G):
-
-    WORKERS = 6
+def test_graph(G, work_num):
     
     workers = []
     queues = []
@@ -73,15 +79,10 @@ def test_graph(G):
     val = Value('i', 2)
     outq = Queue()
 
-    for w in range(WORKERS):
+    for w in range(work_num):
         queues.append(Queue())
 
     start = time.time()
-    
-    for w in range(WORKERS):
-        p = Process(target=calc_max_clique, args=(w, queues[w], outq, val, G,))
-        workers.append(p)
-        p.start()
 
     nodes = G.nodes()
 
@@ -103,7 +104,12 @@ def test_graph(G):
                 if j > v and G.degree(nodes[j]) >= val.value:
                     U.add(nodes[j])
 
-            queues[v % WORKERS].put((U, C))
+            queues[v % work_num].put((U.copy(), C.copy()))
+
+    for w in range(work_num):
+        p = Process(target=calc_max_clique, args=(w, queues[w], outq, val, G,))
+        workers.append(p)
+        p.start()
 
     for qw in queues:
         qw.put(None)
@@ -116,6 +122,8 @@ def test_graph(G):
     while not outq.empty():
         
         clique = outq.get()
+
+        print(clique)
 
         if len(clique) > len(maxclique):
             maxclique = clique
@@ -141,15 +149,18 @@ def load_graph(path):
 
 def main():
 
+    WORKERS = 6
+
     # Tests
     if len(sys.argv) <= 1:
-        g = NX.complete_graph(10)
-        test_graph(g)
+        V = 10
+        g = NX.complete_graph(V)
+        test_graph(g, 2)
         return
     
     # Load a graph
     g = load_graph(sys.argv[1])
-    test_graph(g)
+    test_graph(g, WORKERS)
 
 if __name__ == "__main__":
     main()
