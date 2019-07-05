49f81c6ce3ba8d59a9a31fccb8e31de4d0d82e35
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2019-07-04 23:08:58 -0300

    Separate tests

---
+++ new-algorithm/heavy_test_poc.py
@@ -0,0 +1,25 @@
+import unittest
+import networkx as NX
+from poc import main
+
+def calc_and_compare(G):
+    result = len(main(G))
+    nx_result = NX.graph_clique_number(G)
+    return result == nx_result
+
+default_seed = 1
+
+class ResultTests(unittest.TestCase):
+    def test_result_gnp_50_0_99(self):
+        assert(calc_and_compare(NX.gnp_random_graph(50, 0.99)))
+    def test_result_gnp_100_0_80(self):
+        assert(calc_and_compare(NX.gnp_random_graph(100, 0.80)))
+    def test_result_circulant_graph_100(self):
+        assert(calc_and_compare(NX.circulant_graph(100, [1,2,3])))
+    def test_result_dgm_200(self):
+        assert(calc_and_compare(NX.dorogovtsev_goltsev_mendes_graph(200)))
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
--- test/test_triangles.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-class TestTriangles(unittest.TestCase):
-
-    def test_upper(self):
-        pass
