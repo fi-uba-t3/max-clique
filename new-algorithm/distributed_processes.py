70f2123e70b7e8451af83149e565907fd168a00d
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2019-07-17 22:41:50 -0300

    Measure times hit by triangles optimization

--- new-algorithm/distributed_processes.py
+++ new-algorithm/distributed_processes.py
@@ -22,7 +22,10 @@ def verify_clique(graph, node, degree_sum):
 
 # Returns the max clique size if it is bigger max_already_found_clique 
 # already_accounted_nodes no cuenta node
-def explore (node, _graph, visited, max_already_found_clique_size):
+def explore (node, _graph, visited, max_already_found_clique_size, calls_made, hits_triangle):
+    with calls_made.get_lock():
+        calls_made.value += 1
+    
     subgraph_freezed = _graph.subgraph(list(_graph.neighbors(node)) + [node])
     triangles, degree_sum = compute_triangles (subgraph_freezed, node, visited)
 
@@ -35,8 +38,10 @@ def explore (node, _graph, visited, max_already_found_clique_size):
     clique = []
     for max_expected_clique_size, next_neighbor in triangles.get_t_n_iterator():
         if max_expected_clique_size <= max_already_found_clique_size:
+            with hits_triangle.get_lock():
+                hits_triangle.value += 1
             break
-        new_clique = explore (next_neighbor, subgraph, visited, max_already_found_clique_size)
+        new_clique = explore (next_neighbor, subgraph, visited, max_already_found_clique_size, calls_made, hits_triangle)
         if len(clique) < len(new_clique):
             clique = new_clique
             if max_already_found_clique_size < len(new_clique):
@@ -46,7 +51,7 @@ def explore (node, _graph, visited, max_already_found_clique_size):
     visited[node] = False
     return clique
 
-def worker_main(worker_id, queue_in, queue_out, max_clique_size, graph, visited):
+def worker_main(worker_id, queue_in, queue_out, max_clique_size, graph, visited, calls_made, hits_triangle):
     node_to_visit = queue_in.get()
     graph_ordered_nodes = list(graph.nodes())
     graph_ordered_nodes.reverse()
@@ -56,7 +61,7 @@ def worker_main(worker_id, queue_in, queue_out, max_clique_size, graph, visited)
             graph.remove_node(popped_element)
             popped_element = graph_ordered_nodes.pop()
         if node_to_visit == popped_element and max_clique_size.value < graph.degree(node_to_visit)  + 1:
-            new_clique = explore (node_to_visit, graph, visited, max_clique_size.value)
+            new_clique = explore (node_to_visit, graph, visited, max_clique_size.value, calls_made, hits_triangle)
             with max_clique_size.get_lock():
                 if max_clique_size.value < len(new_clique):
                     max_clique_size.value = len(new_clique)
@@ -78,11 +83,13 @@ def main(graph, work_num):
     queue_out = Queue()
 
     max_clique_size = Value('i', 0)
+    calls_made = Value('i', 0)
+    hits_triangle = Value('i', 0)
     workers = []
 
 
     for worker_id in range(work_num):
-        p = Process(target=worker_main, args=(worker_id, queue_in, queue_out, max_clique_size, graph, visited))
+        p = Process(target=worker_main, args=(worker_id, queue_in, queue_out, max_clique_size, graph, visited, calls_made, hits_triangle))
         workers.append(p)
         p.start()
 
@@ -95,11 +102,14 @@ def main(graph, work_num):
 
     for worker in workers:
         worker.join()
-    count = 0
+
+    # Exhaust the total of cliques received, until the last one
+    count_of_cliques_received = 0
     while len(max_clique) != max_clique_size.value:
         max_clique = queue_out.get()
-        count += 1
+        count_of_cliques_received += 1
 
+    print('({}, {}, {}, {}, {})'.format(count_of_cliques_received, calls_made.value, hits_triangle.value, len(graph.edges()), len(graph.nodes())))
     return max_clique
 
 
