564b5be1b3aafc15152358bfd572d84350bd9582
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2019-07-21 17:05:45 -0300

    Change name of internal var

--- new/src/algorithms/triangles.py
+++ new/src/algorithms/triangles.py
@@ -14,19 +14,19 @@ class Triangles():
 
     def _pop_element(self):
         
-        self.nodes_per_degree = list(map(lambda x: x - 1, self.nodes_per_degree))
+        self.nodes_per_triangles = list(map(lambda x: x - 1, self.nodes_per_triangles))
         ## Remove empty degrees list
-        while self.nodes_per_degree and self.nodes_per_degree[0] == 0:
-            self.nodes_per_degree.pop(0)
+        while self.nodes_per_triangles and self.nodes_per_triangles[0] == 0:
+            self.nodes_per_triangles.pop(0)
 
     def _get_max_clique_size(self, degree):
         
         ## Lists of greater degree should be empty by now
         ## 2 is because of the node in the explore function and the node which we are currently checking in the iterator 
         max_clique_size = degree + 2
-        nodes_per_degree_sliced = self.nodes_per_degree[- degree - 1:]
+        nodes_per_triangles_sliced = self.nodes_per_triangles[- degree - 1:]
         
-        for amount_of_nodes in nodes_per_degree_sliced:
+        for amount_of_nodes in nodes_per_triangles_sliced:
             if amount_of_nodes + 1 >= max_clique_size: # +1 because of the main_node
                 break
             max_clique_size -= 1
@@ -35,13 +35,13 @@ class Triangles():
         
     def get_t_n_iterator(self):
         
-        self.nodes_per_degree = []
+        self.nodes_per_triangles = []
         self.T.reverse()
         previous_value = 0
         
         for list_of_nodes in self.T:
-            self.nodes_per_degree.append(len(list_of_nodes) + previous_value)
-            previous_value = self.nodes_per_degree[-1]
+            self.nodes_per_triangles.append(len(list_of_nodes) + previous_value)
+            previous_value = self.nodes_per_triangles[-1]
         
         self.T.reverse()
         current_list = len(self.T) - 1
