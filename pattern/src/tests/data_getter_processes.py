dd75180c72e23b70bd90f07b03d1248d34aa3531
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-07-22 13:28:20 -0300

    Add data getter for pattern algorithm

--- pattern/src/tests/data_getter_processes.py
+++ pattern/src/tests/data_getter_processes.py
@@ -8,7 +8,7 @@ grandfather_dir = path.dirname(path.dirname(path.abspath(__file__)))
 
 sys.path.append(grandfather_dir)
 
-from algorithms.parallel import main
+from algorithms.TTT.parallel import maxclique
 
 seed = 100
 
@@ -17,5 +17,5 @@ directory = grandfather_dir + '/../graphs/own-graphs'
 for filename in os.listdir(directory):
     if filename.endswith(".txt"):
         G = NX.read_edgelist(directory + '/' + filename)
-        main(G, 4)
+        maxclique(G, 4, True)
 
